d773503846a2d2ac56bcb8a5d1e58b1cbceec70d
DANIEL JUNIOR KIZITO <kizodanjr@gmail.com>
2025-05-24 19:53:57 +0300

    Assigment 2A

---
+++ factorial.py
@@ -0,0 +1,10 @@
+from functools import reduce
+
+# Factorial using lambda
+factorial = lambda n: reduce(lambda x, y: x * y, range(1, n + 1))
+
+# Compute factorial of 5
+result = factorial(5)
+
+# Print using string concatenation
+print("The factorial of 5 is " + str(result))
